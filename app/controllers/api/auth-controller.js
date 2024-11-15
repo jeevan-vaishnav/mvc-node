9439e07052a3389e16657470360c1ceef3944d8c
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-11-15 19:18:06 +0530

    added jsonwebtoken

--- app/controllers/api/auth-controller.js
+++ app/controllers/api/auth-controller.js
@@ -1,35 +1,29 @@
 const { User} =  require('../../models')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {appKey,tokenExpiresIN} = require('../../../config/app')
+
 const InvalidCredentialException = require('../../exceptions/invalid-credentials-exceptions')
 
 class AuthController {
   async login(req, res) {
-
-    // console.log(req.body)
-    /**
-     * req: Get req from client 
-     * res: send response to client
-     */
-    //extract email and password
     const {email, password } = req.body;
 
     const user = await User.findOne({where :{email}})
-
-
    
     if(!user){
       throw new InvalidCredentialException('Invalid credentials');
     }
 
-
-
     if(!await bcrypt.compare(password,user.password)){
       throw new InvalidCredentialException('Invalid credentials');
     }
+    const payload = {id:user.id,email:user.email,firstName:user.firstName,lastName:user.lastName}
+    // const key = require('crypto').randomBytes(64).toString('hex');
+    // console.log(key);
+    const accessToken = jwt.sign(payload,appKey,{expiresIn:tokenExpiresIN})
 
-
-
-    res.send(user);
+    res.send({user,...{accessToken}});
   }
 
   async register(req, res) {
