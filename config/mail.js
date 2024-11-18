5225c38be7971ea07292faa0b2a4a8d49a70043f
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-11-18 23:51:16 +0530

    added mailer
    Ticket#1

---
+++ config/mail.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+module.exports = {
+  connection: process.env.MAIL_CONNECTION || "smtp",
+  from: process.env.MAIL_FROM,
+  smtp: {
+    driver: "smtp",
+    host: process.env.MAIL_HOST,
+    port: process.env.PORT,
+    auth: {
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  },
+};
