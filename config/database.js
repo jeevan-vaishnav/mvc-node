4d1480b247c676b305026d6621f6a3e5e6cf9a90
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-11-21 15:24:12 +0530

    added custom error pages

--- config/database.js
+++ config/database.js
@@ -7,7 +7,8 @@ module.exports = {
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT,
-    'port':process.env.DB_PORT
+    'port':process.env.DB_PORT,
+    "logging":false
   },
   "test": {
     "username": process.env.DB_USER,
